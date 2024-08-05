f447c730ed2bba0751c474d2ca0c692e36344eaf
tanyaporseva <tporseva@inbox.ru>
2024-08-05 09:54:13 +0300

    fix bugs

--- js/storage.js
+++ js/storage.js
@@ -6,9 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  document.querySelectorAll('input[type="range"]').forEach(input => {
+    const savedValue = localStorage.getItem(input.id);
+    if (savedValue !== null) {
+      input.value = savedValue;
+    }
+  });
+
   document.querySelectorAll('[contenteditable="true"]').forEach(element => {
     element.addEventListener('input', () => {
       localStorage.setItem(element.id, element.innerHTML);
+    });
+  });
+
+  document.querySelectorAll('input[type="range"]').forEach(input => {
+    input.addEventListener('input', () => {
+      localStorage.setItem(input.id, input.value);
     });
   });
 });
